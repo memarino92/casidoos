5d5c5535f1cd4177dc501e17b379bb23e22752dc
memarino92 <memarino92@gmail.com>
2022-06-16 22:34:09 -0400

    added in-source tests for vowelMultiplier

--- vowelMultiplier.ts
+++ vowelMultiplier.ts
@@ -9,25 +9,42 @@
 // $ longText('lol', 10)
 // $ 'looooooooool'
 
-const longText = (string: string, multiplier: number): string => {
-    const vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
-    let resultString = '';
-    
-    [...string].forEach((letter) => {
-        if (vowels.indexOf(letter) !== -1) {
-            for (let i = 0; i < multiplier; i++){
-                resultString += letter 
-            }
-        }
-        else {
-            resultString += letter
-        }
-    })
-    
-    return resultString
-}
+export const longText = (string: string, multiplier: number): string => {
+  const vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
+  let resultString = '';
 
-console.log(longText('hello world', 3))
-// heeellooo wooorld
-console.log(longText('lol', 10))
-// looooooooool
+  [...string].forEach((letter) => {
+    if (vowels.indexOf(letter) !== -1) {
+      for (let i = 0; i < multiplier; i++) {
+        resultString += letter;
+      }
+    } else {
+      resultString += letter;
+    }
+  });
+
+  return resultString;
+};
+
+if (import.meta.vitest) {
+  const { it, assert, describe } = import.meta.vitest;
+  describe('longText', () => {
+    it('should multiply one vowel many times by the given multiplier', () => {
+      const input = 'lol';
+      const multiplier = 10;
+      const result = longText(input, multiplier);
+      const expectedResult = 'looooooooool';
+
+      assert.equal(result, expectedResult);
+    });
+
+    it('should multiply multiple vowels many times by the given multiplier', () => {
+      const input = 'hello world';
+      const multiplier = 3;
+      const result = longText(input, multiplier);
+      const expectedResult = 'heeellooo wooorld';
+
+      assert.equal(result, expectedResult);
+    });
+  });
+}
